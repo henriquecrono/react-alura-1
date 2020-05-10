3c82177a52f52f502a135c0c51f48cf252a342c5
henriquecrono <henriquemiranda5@hotmail.com>
2020-05-10 17:46:49 -0300

    func remove autor

--- src/App.js
+++ src/App.js
@@ -1,40 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './App.css';
 import Tabela from './Tabela';
 
-const autores = [
-  {
-    nome: 'Paulo',
-    livro: 'React',
-    preco: '1000'
-  },
-  {
-    nome: 'Daniel',
-    livro: 'Java',
-    preco: '99'
-  },
-  {
-    nome: 'Marcos',
-    livro: 'Design',
-    preco: '150'
-  },
-  {
-    nome: 'Bruno',
-    livro: 'DevOps',
-    preco: '100'
-  },
-  {
-    nome: 'Henrique',
-    livro: 'React/Node',
-    preco: '2000'
+class App extends Component {
+  state = {
+    autores: [
+      {
+        nome: 'Paulo',
+        livro: 'React',
+        preco: '1000'
+      },
+      {
+        nome: 'Daniel',
+        livro: 'Java',
+        preco: '99'
+      },
+      {
+        nome: 'Marcos',
+        livro: 'Design',
+        preco: '150'
+      },
+      {
+        nome: 'Bruno',
+        livro: 'DevOps',
+        preco: '100'
+      },
+      {
+        nome: 'Henrique',
+        livro: 'React/Node',
+        preco: '2000'
+      }
+    ]
   }
-];
 
-function App() {
-  return (
-    <Tabela autores = { autores } />
-  );
+  removeAutor = index => {
+    const { autores } = this.state;
+
+    this.setState({
+      autores: autores.filter((autor, posAtual) => {
+        return posAtual !== index;
+      })
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Tabela autores={this.state.autores} removeAutor={this.removeAutor} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Tabela.js
+++ src/Tabela.js
@@ -17,54 +17,33 @@ const TableHead = () => {
 const TableBody = props => {
     const linhas = props.autores.map((obj, index) => {
         return (
-            <tr>
-                <td>{ obj.nome }</td>
-                <td>{ obj.livro }</td>
-                <td>{ obj.preco }</td>
-                <td><button>Remover</button></td>
+            <tr key={index}>
+                <td>{obj.nome}</td>
+                <td>{obj.livro}</td>
+                <td>{obj.preco}</td>
+                <td>
+                    <button onClick={() => { props.removeAutor(index) }}>Remover</button>
+                </td>
             </tr>
         );
     });
 
     return (
         <tbody>
-            { linhas }
+            {linhas}
         </tbody>
     );
-
-    // return (
-    //     <tbody>
-    //         <tr>
-    //             <td>Paulo</td>
-    //             <td>React</td>
-    //             <td>1000</td>
-    //             <td><button>Remover</button></td>
-    //         </tr>
-    //         <tr>
-    //             <td>João</td>
-    //             <td>Node JS</td>
-    //             <td>500</td>
-    //             <td><button>Remover</button></td>
-    //         </tr>
-    //         <tr>
-    //             <td>Henrique</td>
-    //             <td>Python</td>
-    //             <td>2000</td>
-    //             <td><button>Remover</button></td>
-    //         </tr>
-    //     </tbody>
-    // );
 }
 
 class Tabela extends Component {
     render() {
-        const { autores } = this.props;
+        const { autores, removeAutor } = this.props;
 
         return (
             <div className="App">
                 <table>
                     <TableHead />
-                    <TableBody autores={autores} />
+                    <TableBody autores={autores} removeAutor={removeAutor} />
                 </table>
             </div>
         );
