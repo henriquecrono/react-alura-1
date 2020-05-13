35b1ef178f50354061f207e0de642ad0145949e8
henriquecrono <henriquemiranda5@hotmail.com>
2020-05-12 23:16:26 -0300

    Typography and makeStyles MaterialUI

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,14 +1,33 @@
 import React, { Fragment } from 'react';
 
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
 import Header from '../../Components/Header/Header';
 
+
+const useStyles = makeStyles({
+    title: {
+        textAlign: 'center',
+        color: 'blue'
+    }
+})
+
 const About = () => {
+    const classes = useStyles();
+
     return (
         <Fragment>
             <Header />
-            <div className="container">
-                <h1 className="center-align">Sobre</h1>
-            </div>
+            <Container maxWidth="sm">
+                <Typography className={classes.title} variant="h1" component="h2">
+                    Sobre
+                </Typography>
+                <Typography variant="body2" component="p">
+                    A Casa do Código faz e edita livros.
+                </Typography>
+            </Container>
         </Fragment>
     );
 
